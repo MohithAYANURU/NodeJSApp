aa7b416ebdb7faa3aac1dffecb0243d89e1bd0de
Mohith <mohith.ayanuru@gmail.com>
2025-11-13 11:46:09 +0100

    Express router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import express from "express";
-import { logMiddleware } from "./middleware/logger.js";
-import { getAllUsers, getUserById, createUser } from "./controllers/userController.js";
+import express from "express"
+import { logMiddleware } from "./middleware/logger.js"
+import userRoutes from "./routes/userRoutes.js"  // Import the router
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.json());
+// Global middleware (applies to all routes)
+app.use(express.json())
 
-// Welcome route
-app.get("/", (req, res) => {
-  res.send("Welcome to the Node.js API!");
-});
+// Mount the user router at /users
+app.use('/users', userRoutes)
 
-// User routes
-app.get("/users", logMiddleware, getAllUsers);
-app.get("/users/:id", getUserById);
-app.post("/users", createUser);
+// Optional: Add a welcome route
+app.get('/', (req, res) => {
+	res.json({ 
+		message: "Welcome to the API",
+		endpoints: {
+			users: "/users"
+		}
+	})
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+	console.log(`Server is running on http://localhost:${PORT}`)
+	console.log(`API Documentation:`)
+	console.log(`  GET    /users      - Get all users`)
+	console.log(`  GET    /users/:id  - Get user by ID`)
+	console.log(`  POST   /users      - Create new user`)
+	console.log(`  PUT    /users/:id  - Update user`)
+	console.log(`  DELETE /users/:id  - Delete user`)
+})
